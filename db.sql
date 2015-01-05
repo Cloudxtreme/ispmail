a0fa052ea681f8b3b2bd7117a0041c7224f13de8
Marcel Bischoff <marcel@herrbischoff.com>
2015-01-05 20:56:11 +0100

    Initial local commit

---
+++ db.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE 'mailserver';
+
+GRANT SELECT ON mailserver.*
+TO 'mailuser'@'127.0.0.1'
+IDENTIFIED BY 'fLxsWdf5ABLqwhZr';
+
+CREATE TABLE `virtual_domains` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `virtual_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `virtual_aliases` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `mailserver`.`virtual_domains` (
+  `id` ,
+  `name`
+)
+VALUES (
+  '1', 'example.org'
+);
+
+INSERT INTO `mailserver`.`virtual_users` (
+  `id` ,
+  `domain_id` ,
+  `password` ,
+  `email`
+)
+VALUES (
+  '1', '1', MD5( 'summersun' ) , 'john@example.org'
+);
+
+INSERT INTO `mailserver`.`virtual_aliases` (
+  `id`,
+  `domain_id`,
+  `source`,
+  `destination`
+)
+VALUES (
+  '1', '1', 'jack@example.org', 'john@example.org'
+);
---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# ISPmail Install Script for Ubuntu 14.04 LTS
+#
+# Based on https://workaround.org/book/export/html/447
+# Created by Marcel Bischoff <https://github.com/herrbischoff>
+
+# Update system
+aptitude update && \
+aptitude upgrade -y && \
+aptitude dist-upgrade -y && \
+
+# Install software
+aptitude install ssh postfix postfix-mysql swaks mysql-server dovecot-mysql dovecot-pop3d dovecot-imapd dovecot-managesieved roundcube roundcube-plugins -y && \
+
+# Generate self-signed certificate
+openssl req -new -x509 -days 3650 -nodes -newkey rsa:4096 -out /etc/ssl/certs/mailserver.pem -keyout /etc/ssl/private/mailserver.pem && \
+
+# Setup Apache for SSL
+sed -i 's|/etc/ssl/certs/ssl-cert-snakeoil.pem|/etc/ssl/certs/mailserver.pem|' /etc/apache2/sites-available/default-ssl.conf && \
+sed -i 's|/etc/ssl/private/ssl-cert-snakeoil.key|/etc/ssl/private/mailserver.pem|' /etc/apache2/sites-available/default-ssl.conf && \
+a2ensite default-ssl && \
+a2enmod ssl && \
+service apache2 reload && \
+
+# Setup database
+mysql -u root < db.sql && \
+
+# Setup Postfix
+mv mysql-virtual-mailbox-domains.cf /etc/postfix/ && \
+postconf -e virtual_mailbox_domains=mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf && \
+
+mv mysql-virtual-mailbox-maps.cf /etc/postfix/mysql-virtual-mailbox-maps.cf && \
+postconf -e virtual_mailbox_maps=mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf && \
+
+mv mysql-virtual-alias-maps.cf /etc/postfix/mysql-virtual-alias-maps.cf && \
+postconf -e virtual_alias_maps=mysql:/etc/postfix/mysql-virtual-alias-maps.cf && \
+
+# Check Postfix config
+echo "This should output '1':" && \
+postmap -q example.org mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf && \
+echo "This should output '1':" && \
+postmap -q john@example.org mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf && \
+echo "This should output 'john@example.com':" && \
+postmap -q jack@example.org mysql:/etc/postfix/mysql-virtual-alias-maps.cf && \
+
+# Setup Dovecot
+groupadd -g 5000 vmail && \
+useradd -g vmail -u 5000 vmail -d /var/vmail -m && \
+chown -R vmail:vmail /var/vmail && \
+chmod u+w /var/vmail
